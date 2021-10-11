610578acc853401afd634a20c9ba94f8475cd8a2
rodrigo.hernandez <electro001_@hotmail.com>
2021-10-11 13:59:39 -0500

    adding gitignore

--- VoyageExcercise/Interfaces/IInvoice.cs
+++ VoyageExcercise/Interfaces/IInvoice.cs
@@ -23,5 +23,14 @@ namespace VoyageExcercise.Interfaces
         /// <param name="context">Application Database Context</param>
         /// <returns>An Invoce data model</returns>
         public Invoice GetInvoice(AppDBContext context, int transaction_id);
+
+        /// <summary>
+        /// Method <c>GetRangedInvoice</c> Get an invoice by a range of dates.
+        /// </summary>
+        /// <param name="context">Application Database Context</param>
+        /// <param name="start_date">the start date you want to query</param>
+        /// <param name="start_date">the end date you want to query</param>
+        /// <returns>An Invoce list</returns>
+        public List<Invoice> GetRangedInvoice(AppDBContext context, DateTime start_date, DateTime end_date);
     }
 }
